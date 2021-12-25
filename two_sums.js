fd19f1b7a5b02b3f4e40808fbd8a6603eb865191
lifegrain <terrykho@ymail.com>
2021-12-26 01:11:32 +0700

    create the base of two sums logic test

---
+++ two_sums.js
@@ -0,0 +1,13 @@
+function twoSums (nums, target) {
+    
+}
+
+
+
+// examples
+console.log(twoSums([2,7,11,15], 13)); // expected [0,2]
+console.log(twoSums([3,2,4], 6));   // expected [1,2]
+console.log(twoSums([3,3], 6)); // expected [0,1]
+
+// additional test
+console.log(twoSums([4,5,6,7,9,3,1], 10)); // expected [[0,2],[3,5],[4,6]]
